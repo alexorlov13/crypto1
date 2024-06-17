fc30dc23ee7c5a7d9f1631f2a5ccb0a8e0ee41c9
alexorlov13 <alexorlov013@gmail.com>
2024-06-17 17:20:37 +0300

    1

---
+++ exchanger/data/config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = '7445853952:AAHRTtrcERm6f1Zz-b-Thv6MFy0GiPxA-nA'
+ADMIN_ID = [201861656] #айдишник администратора
+ANKET_SEND = [201861656] #айдишник, куда будут отсылаться заявки на обмен
+SUPPORT_LINK = 'https://t.me/mul' #ссылка на саппорта
+PATH_TO_DB = '/Users/Zenno10/Desktop/exchanger/exchanger/data/' #путь до файла с базой
+LINK_SUBSRIBE = 'https://t.me/mul'
+#faq должно быть не больше 4000 символов!
+FAQ = F'''
+Часто задаваемые вопросы:
+Разработчики: https://t.me/weaseldev @weaseldev'''
